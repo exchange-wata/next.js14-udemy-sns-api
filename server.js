e19097277e70d8f702b524fb950712ca32f2c31d
exchange-wata <exchange.wata0907@gmail.com>
2023-12-08 15:43:00 +0900

    新規登録APIの仮実装

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const { PrismaClient } = require('@prisma/client');
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
 
 const PORT = 5000;
-app.get("/", (req, res) => { res.send("<h1>hello</h1>") })
+const prisma = new PrismaClient
+
+// 新規登録
+app.post("/api/auth/register", async (req, res) => {
+  const { name, email, password } = req.body
+  const hashedPassword = bcrypt.hash(password, 10)
+  const user = await prisma.user.create({
+    body: {
+      name, email, password: hashedPassword
+    }
+  })
+
+  return user.json({ user })
+})
+
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
 
