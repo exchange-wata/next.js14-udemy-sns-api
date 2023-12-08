76646829619bde767636c8042d1975e62cf804ed
exchange-wata <exchange.wata0907@gmail.com>
2023-12-08 17:09:30 +0900

    eslintとprettierを追加

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const { PrismaClient } = require('@prisma/client');
-const express = require('express')
-const app = express()
-const bcrypt = require('bcrypt')
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcrypt';
+import express, { json } from 'express';
+
+const app = express();
 
 const PORT = 5000;
-const prisma = new PrismaClient
+const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(json());
 
 // 新規登録
-app.post("/api/auth/register", async (req, res) => {
-  const { name, email, password } = req.body
-  const hashedPassword = await bcrypt.hash(password, 10)
+app.post('/api/auth/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await hash(password, 10);
   const user = await prisma.user.create({
     data: {
-      name, email, password: hashedPassword
-    }
-  })
+      name, email, password: hashedPassword,
+    },
+  });
 
-  return res.json({ user })
-})
+  return res.json({ user });
+});
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
-
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
