1284156fc18895accfaafc97e486586ff8f8f89e
exchange-wata <exchange.wata0907@gmail.com>
2023-12-08 13:14:04 +0900

    migrationファイル作成

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const express = require('express')
 const app = express()
 
 const PORT = 5000;
+app.get("/", (req, res) => { res.send("<h1>hello</h1>") })
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
+
