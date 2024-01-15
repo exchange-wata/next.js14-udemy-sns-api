7356cbda5e312a53f6b3b4995da2a909f5cb609c
exchange-wata <exchange.wata0907@gmail.com>
2024-01-15 10:37:13 +0900

    corsエラーの解消

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import cors from 'cors';
 import express, { json } from 'express';
-// eslint-disable-next-line import/named, import/extensions
+// eslint-disable-next-line import/extensions
 import { authRoute } from './routers/auth.js';
 
 const app = express();
 
 const PORT = process.env.API_PORT;
 
+app.use(cors());
 app.use(json());
-
 app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
